9ed0171c1e6a8e8d8c89f213dd15b3ccc45e193c
Wüller Barros <105557727+wullerbarros@users.noreply.github.com>
2023-01-21 12:04:33 -0300

    Add files via upload

---
+++ aula001/index.js
@@ -0,0 +1,71 @@
+/*var soma = 100 + + 50;
+var subtracao = 100 - 50;
+var multiplicacao = 100 * 2;
+var divisao = 100/2;
+var expoente = 2 **  4;
+var modulo = 10 % 5;
+
+alert(soma);*/
+
+/*var incremento = 5;
+alert(++incremento);
+alert(incremento);*/
+
+/*let numero1 = prompt("digite um numero");
+let numero2 = prompt("digite outro numero");
+
+alert(numero1 * numero2);*/
+
+
+
+ // let numero1 = '90';
+ // let numero2 = 100
+
+ // alert(numero1 + numero2);
+
+ //var incremento = 10;
+ //alert(++incremento);
+ //alert(incremento);
+
+// let !temMundial = true
+ //alert(!temMundial);
+
+
+ //let nota1 = prompt("digite sua nota");
+ 
+ //let nota2 = prompt("digite sua nota");
+ 
+ //let media = (+nota1  + +nota2)/2
+ //alert(media);
+ //if (media >= 6){
+  //  alert("aluno aprovado", media);
+    
+ //}
+ 
+ //else
+ //alert("aluno reprovado");
+ 
+
+ let peso = prompt("qual seu peso")
+ let altura = prompt("qual sua altura")
+
+ let imc = ( peso / (altura * altura))
+ 
+alert(imc);
+ if( imc < 18.5){
+    alert("magreza")
+
+ }
+ else if (imc > 18.5 && imc <= 24.9){
+    alert("normal")
+
+ }
+ else if (imc >= 25 && imc <= 29.9){
+    alert("sobrepeso")
+ }
+ else if (imc >= 30 && imc <= 39.9){
+    alert("obesidade")
+ }
+ else if(imc > 40){
+    alert("obesidade grave")
+ }
